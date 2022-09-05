e25314c2df40815abbf60501c152f4b814468d38
wuyawei <wuyawei@enn.cn>
2022-09-05 13:12:47 +0800

    vue3-template

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const count = ref(0)
+</script>
+
+<template>
+  <h3>Hello vue3-template</h3>
+  <p>{{ count }}</p>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: 'vue',
+      dts: './src/auto-imports.d.ts'
+    })
+  ]
+})
