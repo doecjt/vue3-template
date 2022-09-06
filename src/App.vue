bfaf3ce17a5d97482f3b5e556e8657bd328ad9bb
wuyawei <wuyawei@enn.cn>
2022-09-06 14:11:05 +0800

    feature: add icon

--- src/App.vue
+++ src/App.vue
@@ -7,4 +7,13 @@ const count = ref(0)
   <p>{{ count }}</p>
   <div class="bg-sky">click</div>
   <div bg-orange>xixi</div>
+  <div
+    i-twemoji-grinning-face-with-smiling-eyes
+    text-80px
+    hover:i-twemoji-face-with-tears-of-joy
+    cursor-pointer
+  />
+  <p
+    text="30px #1578ff"
+  >attributify</p>
 </template>
--- unocss.config.ts
+++ unocss.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'unocss'
 import presetWind from '@unocss/preset-wind'
 import presetAttributify from '@unocss/preset-attributify'
+import presetIcons from '@unocss/preset-icons'
 
 export default defineConfig({
   presets: [
     presetWind(),
     presetAttributify(),
+    presetIcons(),
   ]
 })
