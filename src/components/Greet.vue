e57b396d0dba1e766627b2e28949f866636ad637
wuyawei <wuyawei@enn.cn>
2022-09-30 15:14:28 +0800

    deploy test

--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -3,9 +3,11 @@ import { isDark, toggleDark } from '@/composables/dark'
 </script>
 
 <template>
-  <div i-bi:github text="50px" />
-  <h3>Hello vue3-template</h3>
-  <div cursor-pointer text="30px" @click="toggleDark()">
-    <div i="carbon-sun dark:carbon-moon" />
+  <div pt-40px flex flex-col items-center>
+    <div i-bi:github text="50px" />
+    <h3>Hello vue3-template</h3>
+    <div cursor-pointer text="30px" @click="toggleDark()">
+      <div i="carbon-sun dark:carbon-moon" />
+    </div>
   </div>
 </template>
