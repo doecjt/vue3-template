49e142fbb72b4602a4d3a457e0f19fbd20e7b402
wuyawei <wuyawei@enn.cn>
2023-01-31 18:16:48 +0800

    feat: update eslint-config

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: '@antfu',
+  rules: {
+    'no-console': 'off',
+    'curly': ['error', 'all'],
+    '@typescript-eslint/brace-style': 'off',
+  },
+}
--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import Greet from '@/components/Greet.vue'
-
-const a = 1
 </script>
 
 <template>
